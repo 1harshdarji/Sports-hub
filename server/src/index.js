3cdbc389297d2a933112202c6e9ef069cadc929c
Harsh <harshdarji502@gmail.com>
2025-12-24 12:19:24 +0530

    README

--- server/src/index.js
+++ server/src/index.js
@@ -38,26 +38,24 @@ const PORT = process.env.PORT || 5000;
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization']
 }));*/
-const allowedOrigins = [
-    "https://sports-hub-nu.vercel.app",
-    "http://localhost:5173"
-];
+app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "https://sports-hub-nu.vercel.app");
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    res.header(
+        "Access-Control-Allow-Methods",
+        "GET, POST, PUT, PATCH, DELETE, OPTIONS"
+    );
 
-app.use(cors({
-    origin: function (origin, callback) {
-        if (!origin || allowedOrigins.includes(origin)) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"]
-}));
+    if (req.method === "OPTIONS") {
+        return res.sendStatus(200);
+    }
 
-app.options("*", cors());
-
+    next();
+});
 
 // Parse JSON bodies
 app.use(express.json());
